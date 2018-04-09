c49d0216345b7e1da818a352ca7e490c4aa3e774
Shauna <shauna.kerr@gmail.com>
2018-04-09 13:31:51 -0400

    Add jQuery

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var memoryGame = {
     init: function(){
-        let canvas = document.getElementById("canvas");
+        const canvas = document.getElementById("canvas");
         // build grid of cards
         let faceDown = [];
         let faceUp = [];
@@ -12,36 +12,53 @@ var memoryGame = {
 
     },
 
-    // get user grid size
+    // getGridSize is called when user Starts the game (via button)
     getGridSize: function(event){
         event.preventDefault();
         event.stopPropagation();
         event.stopImmediatePropagation();
 
-        const width = document.getElementById("width").value;
-        const height = document.getElementById("height").value;
+        const numPairs = document.getElementById("pairs").value;
 
-        // make the board
-        memoryGame.displayBoard(width, height);
+        memoryGame.displayBoard(numPairs);
     },
 
-    displayBoard: function(width, height){
+    // Shuffle function from http://stackoverflow.com/a/2450976
+    shuffleCards: function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    },
+
+    displayBoard: function(numPairs){
+        console.log("displayBoard is running");
         // clear the canvas!
         canvas.innerHTML = "";
 
         // HTML for each card
         const board = document.createDocumentFragment();
 
-        // for each row, create row
-        for (let ri = 0; ri < height; ri++ ){
-            let row = document.createElement("tr");
 
-            // append cells to row
-            for ( let cin = 0; cin < width; cin++ ){
-                let cell = document.createElement("td");
-                cell.innerHTML =
-                row.appendChild( cell );
-            }
+        // for each row, create row
+        // for (let ri = 0; ri < height; ri++ ){
+        //     let row = document.createElement("tr");
+
+        //     // append cells to row
+        //     for ( let cin = 0; cin < width; cin++ ){
+        //         let cell = document.createElement("td");
+        //         cell.innerHTML = "hi";
+        //         row.appendChild( cell );
+        //     }
+
+        // shuffle the cards
 
             // append row to board
             board.appendChild(row);
@@ -73,4 +90,4 @@ var memoryGame = {
 
 };
 
-memoryGame.init();
+$( document ).ready( memoryGame.init() );
